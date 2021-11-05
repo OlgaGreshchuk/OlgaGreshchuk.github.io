30c70d0037fcc5b30f34fa4b22068e62f4c536fa
OlgaGreshchuk <greolv15@gmail.com>
2021-11-05 23:18:06 +0200

    fix bug with pressure

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ document.getElementById("find_city").addEventListener("click", () => {
                     console.log(data5);
 
                     const pressure_every3hours = document.getElementById("pressure_every3hours");
-                    
+                    pressure_every3hours.innerHTML = '';
 
                    for (let i=0; i<40; i++){
                         const time_every3hours = document.getElementById(`time${i}`);
@@ -102,6 +102,7 @@ document.getElementById("find_city").addEventListener("click", () => {
                         wind_every3hours.textContent = Math.round(data5.list[i].wind.speed) + " м/с";
                         
                         //Додаєм інформацію про атмосферний тиск
+                            
                         const pressure_item = document.createElement("div");
                         pressure_item.textContent = convertPressure(data5.list[i].main.pressure) + " мм.рт.ст.";
                         pressure_every3hours.appendChild(pressure_item);
